b216285e7688157c06ce953a306f7cfc9f5205e8
sup <sup@supui-noteubug.local>
2023-03-25 17:42:04 +0900

    test

---
+++ FirstXcodeTest/FirstXcodeTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstXcodeTestApp.swift
+//  FirstXcodeTest
+//
+//  Created by sup on 2023/03/12.
+//
+
+import SwiftUI
+import UIKit
+
+@main
+struct FirstXcodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
